a1063d21224a35a7eaf57f0aab3177914160cafd
androidkkn <kauan.kkn.2020@gmail.com>
2026-03-20 10:40:42 -0300

    Auto backup de fim de expediente: Fri 03/20/2026 10:40:42.04

---
+++ zAUTO/PULL_PUSH/auto-pull.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Atualizando o repositorio...
+cd C:\Users\1DEVSESI46A\Documents\Kauan_Aulas
+git pull origin main
---
+++ zAUTO/PULL_PUSH/auto-push.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Salvando seu codigo no GitHub...
+
+cd C:\Users\1DEVSESI46A\Documents\Kauan_Aulas
+
+:: Adiciona todas as modificacoes
+git add .
+
+:: Faz o commit com a data e hora atual
+git commit -m "Auto backup de fim de expediente: %date% %time%"
+
+:: Faz o push
+git push origin main
+
+echo.
+echo Push concluido! Saindo do perfil em 5 segundos...
+
+:: Pausa de 5 segundos para voce ler a mensagem
+timeout /t 10
+
+:: Comando para Fazer Logoff
+shutdown /l
